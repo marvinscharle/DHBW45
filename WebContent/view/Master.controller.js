1dd0398c8a1d3dc0b521aa8f0b3f8017e82b5e7d
marvinscharle <marvin.scharle@conclurer.com>
2015-03-05 17:50:37 +0100

    Suche ist nun funktionsfähig

--- WebContent/view/Master.controller.js
+++ WebContent/view/Master.controller.js
@@ -38,8 +38,39 @@ sap.ui.core.mvc.Controller.extend("DHBW.view.Master", {
 	//Suche
 	
 	handleSearch: function(oEvt) {
+		var sQuery = oEvt.getSource().getValue().toLowerCase();
 
-		var aFilters = [];
+		$.each($('#__xmlview1--list').find('li'), function (i, element) {
+			var number = $(element).find('.sapMObjLNumber')[0];
+			if (!number) {
+				number = '';
+			} else {
+				number = number.innerText;
+			}
+
+			var name = $(element).find('.sapMTextLineClamp')[0];
+			if (!name) {
+				name = '';
+			} else {
+				name = name.innerText.toLowerCase();
+			}
+
+			if (sQuery == '') {
+				// Suche leer -> alle Elemente anzeigen
+				$(element).css("display", "block");
+				return;
+			}
+
+			if (number.indexOf(sQuery) > -1 || name.indexOf(sQuery) > -1) {
+				// String gefunden -> Element zeigen
+				$(element).css('display', 'block');
+			}
+			else {
+				$(element).css('display', 'none');
+			}
+		});
+
+		/* Suche serverseitig, falls irgendwann einsatzfähig var aFilters = [];
 		var sQuery = oEvt.getSource().getValue();
 		if (sQuery && sQuery.length > 0) {
 			var filter = new sap.ui.model.Filter("Nname", sap.ui.model.FilterOperator.Contains, sQuery);
@@ -49,7 +80,7 @@ sap.ui.core.mvc.Controller.extend("DHBW.view.Master", {
 		// update list binding
 		var list = this.getView().byId("list");
 		var binding = list.getBinding("items");
-		binding.filter(aFilters, "Application");
+		binding.filter(aFilters, "Application");*/
 	},
 	
 	handleSelect: function(oEvent) {
